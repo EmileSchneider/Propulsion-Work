c797297ccfe892685543fc304fce2a2fba1ca244
Emile Schneider <emileschneider7@gmail.com>
2017-09-19 17:09:22 +0200

    third commit

--- trivial.js
+++ trivial.js
@@ -1,25 +1,43 @@
 class Game {
   constructor(questions) {
+    this.players = [];
     this.questions = questions;
     this.isRunning = true;
   }
-  askQuestion(n) {
-    var a = prompt(this.questions[n].question, this.questions[n].answers);
+  addPlayer(player) {
+    this.players.push(player);
+  }
+  askQuestion(n, player) {
+    console.log(player);
+    var a = prompt("Question for "+ player.name + " " + this.questions[n].question, this.questions[n].answers);
+
     if(a == null) {
       this.isRunning = false;
     }
     else if(a != this.questions[n].correctAnswer) {
       alert("Wrong Answer, try again")
-      this.askQuestion(n);
+      this.askQuestion(n, player);
+      player.errors++;
     } else {
+      player.score++;
       alert("You little genius you, correct Answer");
     }
   }
   play() {
-    var n = 0;
-    while(n < this.questions.length && this.isRunning){
-      this.askQuestion(n);
-      n++;
+    var questionIndex = 0;
+    var playerIndex = 0;
+    while(questionIndex < this.questions.length && this.isRunning){
+      this.askQuestion(questionIndex, this.players[playerIndex]);
+      playerIndex++;
+      if(!(playerIndex < this.players.length)){
+        playerIndex = 0;
+      }
+      questionIndex++;
+    }
+    if(!this.isRunning){
+      alert("You stopped the game. goodbye")
+    } else {
+      alert(this.players.forEach(function(player){alert(`${player.name} made ${player.errors} mistakes`)}));
     }
   }
 }
@@ -32,6 +50,14 @@ class QuestionObj {
   }
 }
 
+class Player {
+  constructor(name){
+    this.name = name;
+    this.score = 0;
+    this.errors = 0;
+  }
+}
+
 var quest1 = new QuestionObj("What is Air?", ["A","B","C","D"], "B");
 var quest2 = new QuestionObj("What is Love?", ["A","B","C","D"], "C");
 var quest3 = new QuestionObj("What is Hate?", ["A","B","C","D"], "D");
@@ -41,4 +67,9 @@ var quest6 = new QuestionObj("What is Brexit?", ["A","B","C","D"], "C");
 var quest7 = new QuestionObj("What is Greece?", ["A","B","C","D"], "B");
 var listOfQuestions = [quest1, quest2, quest3, quest4, quest5, quest6, quest7];
 
-var game = new Game(listOfQuestions)
+var game = new Game(listOfQuestions);
+var player = new Player("Emile");
+var player2 = new Player("Julya");
+game.addPlayer(player);
+game.addPlayer(player2);
+game.play();
